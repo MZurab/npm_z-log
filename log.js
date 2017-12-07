43aa822518e43bcf66d66dba9a9499c6924ec2c5
RammanAdmin <zurab@ramman.net>
2017-12-07 16:32:55 +0300

    @

--- log.js
+++ log.js
@@ -97,7 +97,7 @@ function defaultMessage (type, id,code,text,v3,v4,v5,v6,v7,v8) {
   };
   if(typeof text != 'undefined') objForViewError['text'] = text;
 
-  var v1 = 'z-log error ('+id+' - '+code+')';
+  var v1 = 'z-log <'+type.toUpperCase()+'> ('+id+' - '+code+')';
   var v2 = text;
   // show error
   printObject (v1,v2,v3,v4,v5,v6,v7,v8,type);
@@ -115,7 +115,9 @@ function defaultWarnMessage ( id,code,text,v3,v4,v5,v6,v7,v8) {
 }
 _['defaultWarnMessage'] = defaultWarnMessage;
 
-if (typeof window == 'object') {
-
+function step ( id,code,text,v3,v4,v5,v6,v7,v8) {
+  return defaultMessage('log',id,code,text,v3,v4,v5,v6,v7,v8);
 }
+_['step'] = step;
+
 module.exports = _;
